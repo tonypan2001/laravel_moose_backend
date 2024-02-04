e3154e10294a01e495aece5f959f1827157a23d7
Prompan Uechanwech <panzaza9543@gmail.com>
2024-02-04 11:24:36 +0700

    first commit

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,143 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use Validator;
+
+class AuthController extends Controller
+{
+    /**
+    * Create user
+    *
+    * @param  [string] name
+    * @param  [string] email
+    * @param  [string] password
+    * @param  [string] password_confirmation
+    * @return [string] message
+    */
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string',
+            'email'=>'required|string|unique:users',
+            'password'=>'required|string|min:8',
+            'c_password' => 'required|same:password'
+        ]);
+
+        $user = new User([
+            'name'  => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+
+        if($user->save()){
+            $tokenResult = $user->createToken('Personal Access Token');
+            $token = $tokenResult->plainTextToken;
+
+            return response()->json([
+            'message' => 'Successfully created user!',
+            'accessToken'=> $token,
+            ],201);
+        }
+        else{
+            return response()->json(['error'=>'Provide proper details']);
+        }
+    }
+
+    public function update(Request $request, User $user)
+    {
+        $request->validate([
+            'name' => 'required|string',
+            'password'=>'required|string|min:8',
+            'c_password'=>'required|same:password'
+        ]);
+
+        if ($request->has('name')) {
+            $user->name = $request->name;
+        }
+        
+        if ($request->has('password')) {
+            $user->password = bcrypt($request->password);
+        }
+
+        if ($user->save())
+        {
+            return response()->json([
+                'message'=>'Update user information successfully.',
+                'user'=>$user
+            ]);
+        } else {
+            return response()->json([
+                'error'=>'Unable to update user information',
+            ], 500);
+        }
+    }
+
+    /**
+    * Login user and create token
+    *
+    * @param  [string] email
+    * @param  [string] password
+    * @param  [boolean] remember_me
+    */
+
+    public function login(Request $request)
+    {
+        $request->validate([
+        'email' => 'required|string|email',
+        'password' => 'required|string',
+        'remember_me' => 'boolean'
+        ]);
+
+        $credentials = request(['email','password']);
+        if(!Auth::attempt($credentials))
+        {
+        return response()->json([
+            'message' => 'Unauthorized'
+        ],401);
+        }
+
+        $user = $request->user();
+        $tokenResult = $user->createToken('Personal Access Token');
+        $token = $tokenResult->plainTextToken;
+
+        return response()->json([
+        'message' => 'Login successful',
+        'data' => $user,
+        'accessToken' =>$token,
+        'token_type' => 'Bearer',
+        ]);
+    }
+
+    /**
+    * Get the authenticated User
+    *
+    * @return [json] user object
+    */
+    public function user(Request $request)
+    {
+        return response()->json($request->user());
+    }
+
+    /**
+    * Logout user (Revoke the token)
+    *
+    * @return [string] message
+    */
+    public function logout(Request $request)
+    {
+        $request->user()->tokens()->delete();
+
+        return response()->json([
+        'message' => 'Successfully logged out'
+        ]);
+
+    }
+
+    public function userList(User $user) {
+        return $user;
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+
+Route::group(['prefix' => 'auth'], function () {
+    Route::post('login', [AuthController::class, 'login']);
+    Route::post('register', [AuthController::class, 'register']);
+    Route::get('users', [UserController::class, 'index']);
+
+    Route::group(['middleware' => 'auth:sanctum'], function() {
+      Route::get('logout', [AuthController::class, 'logout']);
+      Route::get('user', [AuthController::class, 'user']);
+      Route::put('update/{user}', [AuthController::class, 'update']);
+    });
+});
